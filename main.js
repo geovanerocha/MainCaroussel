adad6c994d361413e675952b819ed541e6f116bc
Geovane Rocha <unkgfx@gmail.com>
2013-08-07 14:16:44 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+var MainCaroussel = {
+
+	_init: function(){
+		MainCaroussel._nav();
+	},
+
+	_next: function(){
+		caroussel.animate({left: "-="+widthLi}, 500);
+		arrowPrevious.show();
+		index++;
+		if(index == carousselLimit){
+		    arrowNext.hide();
+		}else{
+		    arrowNext.show();
+		}
+	},
+
+	_previous: function(){
+		caroussel.animate({left: "+="+widthLi}, 500);
+		index--;
+		if(index == 1){
+			arrowPrevious.hide();
+		}else{
+			arrowPrevious.show();
+			arrowNext.show();
+		}
+	},
+
+	_nav: function() {
+
+		caroussel     = $('#caroussel-list');
+		carousselLeft = parseInt(caroussel.css('left'));
+		carousselItem = caroussel.find('li');
+		widthLi       = carousselItem.width();
+		carousselLimit        = carousselItem.length;
+		index = 1;
+		arrowNext  = $('.arrow-next');
+		arrowPrevious = $('.arrow-previous');
+
+		arrowNext.on('click', MainCaroussel._next);
+		arrowPrevious.on('click', MainCaroussel._previous);
+
+
+	}
+}
+
+	MainCaroussel._init();
+
+});
