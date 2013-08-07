39998834962ecd40d79051588df27c6b3d3ebe7e
Geovane Rocha <unkgfx@gmail.com>
2013-08-07 14:24:31 -0300

    identação

--- js/main.js
+++ js/main.js
@@ -29,14 +29,14 @@ var MainCaroussel = {
 
 	_nav: function() {
 
-		caroussel     = $('#caroussel-list');
-		carousselLeft = parseInt(caroussel.css('left'));
-		carousselItem = caroussel.find('li');
-		widthLi       = carousselItem.width();
-		carousselLimit        = carousselItem.length;
-		index = 1;
-		arrowNext  = $('.arrow-next');
-		arrowPrevious = $('.arrow-previous');
+		caroussel      = $('#caroussel-list');
+		carousselLeft  = parseInt(caroussel.css('left'));
+		carousselItem  = caroussel.find('li');
+		widthLi        = carousselItem.width();
+		carousselLimit = carousselItem.length;
+		index          = 1;
+		arrowNext      = $('.arrow-next');
+		arrowPrevious  = $('.arrow-previous');
 
 		arrowNext.on('click', MainCaroussel._next);
 		arrowPrevious.on('click', MainCaroussel._previous);
